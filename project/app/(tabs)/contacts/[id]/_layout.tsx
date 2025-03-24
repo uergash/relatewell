83fe61cfb763d5bc03fbfd8ee43c783b40c7d600
Ubek Ergashev <ubek.ergashev@gmail.com>
2025-03-24 19:56:01 -0400

    Delete project directory

--- project/app/(tabs)/contacts/[id]/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from 'expo-router';
-
-export default function ContactDetailLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-    </Stack>
-  );
-}
